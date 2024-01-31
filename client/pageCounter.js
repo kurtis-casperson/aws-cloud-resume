51ad0233c15941a09493ac18b87d906189458a7c
Kurtis <kcasperson7@gmail.com>
2024-01-31 14:02:22 -0800

    Add styles to show page count

--- client/pageCounter.js
+++ client/pageCounter.js
@@ -1,13 +1,10 @@
-const pageCount = document.querySelector('count')
-
-// get the current page count from dynamo
+const pageCount = document.getElementById('count')
 
 async function updateCounter() {
   let response = await fetch(
     'https://65gecqx6qtjqqjdazen3f6jx7y0vxxae.lambda-url.us-west-1.on.aws/'
   )
   let data = await response.json()
-  console.log('data', data)
   pageCount.innerHTML = `${data}`
 }
 
