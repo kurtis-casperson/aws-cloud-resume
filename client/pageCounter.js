d7da5a474aaf7bf6deac9b7fd10df5661a44594d
Kurtis <kcasperson7@gmail.com>
2024-04-07 21:46:28 -0700

    Update lambda function

--- client/pageCounter.js
+++ client/pageCounter.js
@@ -2,7 +2,7 @@ const pageCount = document.getElementById('count')
 
 async function updateCounter() {
   let response = await fetch(
-    'https://65gecqx6qtjqqjdazen3f6jx7y0vxxae.lambda-url.us-west-1.on.aws/'
+    'https://x3m3uc4qjrm6znypf4reyfqtti0cfqpv.lambda-url.us-west-1.on.aws/'
   )
   let data = await response.json()
   pageCount.innerHTML = `${data}`
